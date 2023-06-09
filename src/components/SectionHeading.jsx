d6021c5f0be718f9a55def3acc27a87ba243a32a
dasoib <dasojib49@gmail.comm>
2023-06-09 11:34:15 +0600

    popular section done

---
+++ src/components/SectionHeading.jsx
@@ -0,0 +1,11 @@
+
+const SectionHeading = ({heading,subHeading}) => {
+    return (
+        <div className="my-10 md:w-1/2 text-center mx-auto">
+            <h3 className="text-4xl uppercase border-y-8 py-4 text-red-400">----- {heading} ----</h3>
+            <p className="mb-4 text-purple-500 text-xl mt-3">{subHeading}</p>
+        </div>
+    );
+};
+
+export default SectionHeading;
