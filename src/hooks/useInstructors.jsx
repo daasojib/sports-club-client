8e253869f9aad2f57c36be31e0ff2a6f2e72212a
dasoib <dasojib49@gmail.comm>
2023-06-10 08:50:40 +0600

    classes json done

---
+++ src/hooks/useInstructors.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useInstructors = () => {
+  const [instructors, setInstructors] = useState([]);
+  const [loading, setLoading] = useState([]);
+  useEffect(() => {
+    fetch("instructors.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setInstructors(data);
+        setLoading(false);
+      });
+  }, []);
+  return [instructors, loading];
+};
+
+export default useInstructors;
