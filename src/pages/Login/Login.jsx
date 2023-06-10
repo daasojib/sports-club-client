faccdcc76e4b8538bd196f42ad14f1f4ac3bd895
dasoib <dasojib49@gmail.comm>
2023-06-11 00:55:58 +0600

    regestration page done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,35 +1,41 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const {logIn} = useContext(AuthContext)
+  const { logIn } = useContext(AuthContext);
 
-  const handleLogin = e => {
-    e.preventDefault()
+  const onSubmit = (e) => {
+    e.preventDefault();
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
-    logIn(email, password)
-      .then(data => {
-        const user = data.user;
-        console.log(user)
-    })
-
-  }
+    logIn(email, password).then((data) => {
+      const user = data.user;
+      console.log(user);
+    });
+  };
 
   return (
     <div className="hero min-h-screen bg-base-200 w-3/4 mx-auto">
       <div className="hero-content flex-col">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold text-center">Login now!</h1>
+          <h1 className="text-5xl font-bold text-center">Login!!</h1>
           <p className="py-6">
-           Please Login to <span className="text-red-600 font-bold">Sports Club</span> to access the best sports training and learning environment.
+            Please Login to{" "}
+            <span className="text-red-600 font-bold">Sports Club</span> to
+            access the best sports training and learning environment.
           </p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -37,9 +43,13 @@ const Login = () => {
               <input
                 type="email"
                 name="email"
+                {...register("email", { required: true })}
                 placeholder="email"
                 className="input input-bordered"
               />
+              {errors.email && (
+                <span className="text-red-400">Email is required</span>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -48,15 +58,21 @@ const Login = () => {
               <input
                 type="password"
                 name="password"
+                {...register("password", { required: true })}
                 placeholder="password"
                 className="input input-bordered"
               />
+              {errors.password && (
+                <span className="text-red-400">Password is required</span>
+              )}
             </div>
             <div className="form-control mt-6">
               <input className="btn btn-primary" type="submit" value="Login" />
             </div>
           </form>
-          <p className="p-5 text-center text-red-500">New here?<Link to='/signup'> Create an account</Link></p>
+          <p className="p-5 text-center">
+            New here?<Link className="text-primary font-bold" to="/signup"> Create an account</Link>
+          </p>
         </div>
       </div>
     </div>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,122 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    return (
-        <div>
-            
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200 w-3/4 mx-auto">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold text-center">SignUp!!</h1>
+          <p className="py-6">
+            Please SignUp to
+            <span className="text-red-600 font-bold"> Sports Club</span> to
+            access the best sports training and learning environment.
+          </p>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                {...register("name", { required: true })}
+                placeholder="Name"
+                className="input input-bordered"
+              />
+              {errors.name && (
+                <span className="text-red-500">Name is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                {...register("email", { required: true })}
+                placeholder="Email"
+                className="input input-bordered"
+              />
+              {errors.email && (
+                <span className="text-red-500">Email is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                name="password"
+                {...register("password", {
+                  required: true,
+                  maxLength: 6,
+                  pattern: /(?=.*[0-9])(?=.*[a-z])/,
+                })}
+                placeholder="Password"
+                className="input input-bordered"
+              />
+              {errors.password?.type === "required" && (
+                <p className="text-red-500">Password is required</p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-500">
+                  You Have to give a small letter and a number minimum
+                </p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p className="text-red-500">
+                  Password must be less than 6 character
+                </p>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Confirm Password</span>
+              </label>
+              <input
+                type="text"
+                name="confirmPassword"
+                {...register("password", { required: true })}
+                placeholder="Confirm Password"
+                className="input input-bordered"
+              />
+              {errors.password?.type === "required" && (
+                <p className="text-red-500">Password is required</p>
+              )}
+            </div>
+            <div className="form-control mt-6">
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Sign Up"
+              />
+            </div>
+          </form>
+          <p className="p-5 text-center">
+            Already Have an account?{" "}
+            <Link className="text-primary font-bold" to="/login">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
